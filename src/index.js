6b697cd82022bde82ba44ff99d543239385cc412
ayan4m1 <andrew@bulletlogic.com>
2021-01-24 20:33:02 -0500

    set cache if not cached

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const useGeoPattern = (hash, options) => {
   var isCached = cache.has(hash);
   var pattern = isCached ? cache.get(hash) : GeoPattern.generate(hash, options);
 
+  if (!isCached) {
+    cache.set(hash, pattern);
+  }
+
   return {
     'background-image': pattern.toDataUrl()
   };
