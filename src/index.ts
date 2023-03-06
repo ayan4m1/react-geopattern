6136a2c3c4b9628d7b08b7d52003cce9161d72bd
ayan4m1 <andrew@bulletlogic.com>
2023-03-05 22:36:54 -0500

    allow consumer to override cache

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { generate, Pattern, PatternOptions } from 'geopattern';
 
-const cache = new Map<string, Pattern>();
+export type CacheMap = Map<string, Pattern>;
 
 /**
  * Represents an object with CSS style properties.
@@ -9,21 +9,21 @@ export interface StyleObject {
   'background-image'?: string;
 }
 
+const moduleCache: CacheMap = new Map<string, Pattern>();
+
 /**
  * Create and cache or return cached GeoPattern object.
  *
- * @param string The cache key / data to hash.
- * @param options PatternOptions, see GeoPattern docs.
+ * @param string? The cache key / data to hash.
+ * @param options? PatternOptions, see GeoPattern docs.
+ * @param cache? Map to use as a cache
  * @returns Null if no hash was passed in, a Pattern object otherwise.
  */
 export const useGeoPattern = (
   string: string,
-  options?: PatternOptions
+  options?: PatternOptions,
+  cache: CacheMap = moduleCache
 ): Pattern => {
-  if (!string) {
-    return null;
-  }
-
   const isCached = cache.has(string);
   const pattern = isCached ? cache.get(string) : generate(string, options);
 
@@ -41,5 +41,5 @@ export const useGeoPattern = (
  * @returns A CSS style object.
  */
 export const computePatternStyle = (pattern: Pattern): StyleObject => ({
-  'background-image': pattern?.toDataUrl()
+  'background-image': pattern.toDataUrl()
 });
